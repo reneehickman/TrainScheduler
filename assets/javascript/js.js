cbe82f4588633b13829916ce888f7907c2431c9f
reneecosmic <renee.hickman@outlook.com>
2019-01-22 19:15:59 -0600

    initial on click and variables defined, firebase initalized

---
+++ assets/javascript/js.js
@@ -0,0 +1,32 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCuaAUbIPEquvPkwfFin3SAJiLjFk9aegU",
+    authDomain: "trainscheduler-12287.firebaseapp.com",
+    databaseURL: "https://trainscheduler-12287.firebaseio.com",
+    storageBucket: "trainscheduler-12287.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//button for adding Employees
+$('#submitButton').on('click', function (event) {
+    event.preventDefault();
+
+    //grabs user input
+    var trainName = $('#trainNameForm').val().trim();
+    var destination = $('#trainDestinationForm').val().trim();
+    var firstTrainTime = moment($('#firstTrainTimeForm').val().trim(), "HH:mm").format("HH:mm");
+    var frequency = $('#frequencyForm').val().trim();
+
+    //create local temp object for holding employee data
+    var newTrain = {
+        name: trainName,
+        trainDest: destination,
+        firstTrain: firstTrainTime,
+        trainFreq: frequency
+    };
+
+
+});
