02406af44744d837f988a3b01904262d62ff48fc
reneecosmic <renee.hickman@outlook.com>
2019-01-22 19:18:29 -0600

    initial on click and variables defined, firebase initalized

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -10,17 +10,17 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//button for adding Employees
-$('#submitButton').on('click', function (event) {
+//button for adding a new train
+$("#submitButton").on('click', function (event) {
     event.preventDefault();
 
     //grabs user input
-    var trainName = $('#trainNameForm').val().trim();
-    var destination = $('#trainDestinationForm').val().trim();
-    var firstTrainTime = moment($('#firstTrainTimeForm').val().trim(), "HH:mm").format("HH:mm");
-    var frequency = $('#frequencyForm').val().trim();
+    var trainName = $("#trainNameForm").val().trim();
+    var destination = $("#trainDestinationForm").val().trim();
+    var firstTrainTime = moment($("#firstTrainTimeForm").val().trim(), "HH:mm").format("HH:mm");
+    var frequency = $("#frequencyForm").val().trim();
 
-    //create local temp object for holding employee data
+    //create local temp object for holding train data
     var newTrain = {
         name: trainName,
         trainDest: destination,
